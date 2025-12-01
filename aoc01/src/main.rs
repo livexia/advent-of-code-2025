15b5f91c9efb203eb78ce8589eb8c6fb70a4fd4e
livexia <istillalive@live.cn>
2025-12-01 15:46:51 +0800

    day 1: done

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -53,13 +53,18 @@ fn part2(rotations: &[i32]) -> Result<u32> {
     for rot in rotations {
         // 计算一定会经过 0 刻度的整圈次数
         password += rot.unsigned_abs() / 100;
+
+        // 扣除整圈的旋转距离，
+        // 可以规避例如从 0 刻度旋转距离 100 的边界情况
         let rot = rot % 100;
+
         let temp = dial + rot;
         // 当起点不是 0 刻度时，
         // 向左或向右旋转超过或位于刻度边界 0 或 100 时，
         // 一定经过一次 0 刻度
         password += (dial != 0 && (temp >= 100 || temp <= 0)) as u32;
 
+        // 计算旋转后刻度的真实位置，刻度值一定大于 0
         dial = temp.rem_euclid(100);
     }
 
