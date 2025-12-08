a687491eb2cba43e520ac3f2d8b9684796f1432a
livexia <istillalive@live.cn>
2025-12-08 17:01:25 +0800

    day 8: with union find

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::error::Error;
 use std::io::{self, Read};
 use std::time::Instant;
@@ -11,9 +10,10 @@ macro_rules! err {
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
 type Coord = (usize, usize, usize);
+type Edge = (usize, usize);
 
-fn parse_input<T: AsRef<str>>(input: T) -> Result<Vec<Coord>> {
-    input
+fn parse_input<T: AsRef<str>>(input: T) -> Result<(Vec<Coord>, Vec<Edge>)> {
+    let coords: Vec<Coord> = input
         .as_ref()
         .lines()
         .filter(|l| !l.trim().is_empty())
@@ -28,17 +28,12 @@ fn parse_input<T: AsRef<str>>(input: T) -> Result<Vec<Coord>> {
                 _ => err!("unable parse input: {l:?}"),
             }
         })
-        .collect()
+        .collect::<Result<_>>()?;
+    let connections = generate_connections(&coords);
+    Ok((coords, connections))
 }
 
-fn distance(c: Coord, other: Coord) -> usize {
-    let dx = c.0.abs_diff(other.0);
-    let dy = c.1.abs_diff(other.1);
-    let dz = c.2.abs_diff(other.2);
-    dx.pow(2) + dy.pow(2) + dz.pow(2)
-}
-
-fn connections(coords: &[Coord]) -> Vec<(usize, usize)> {
+fn generate_connections(coords: &[Coord]) -> Vec<Edge> {
     let mut edges = vec![];
     for i in 0..coords.len() {
         for j in i + 1..coords.len() {
@@ -46,9 +41,15 @@ fn connections(coords: &[Coord]) -> Vec<(usize, usize)> {
         }
     }
 
-    edges.sort_by_key(|(a, b)| distance(coords[*a], coords[*b]));
-
+    edges.sort_unstable_by_key(|(a, b)| distance(coords[*a], coords[*b]));
     edges
+}
+
+fn distance(c: Coord, other: Coord) -> usize {
+    let dx = c.0.abs_diff(other.0);
+    let dy = c.1.abs_diff(other.1);
+    let dz = c.2.abs_diff(other.2);
+    dx.pow(2) + dy.pow(2) + dz.pow(2)
 }
 
 fn find(circuits: &mut [usize], i: usize) -> usize {
@@ -62,35 +63,33 @@ fn find(circuits: &mut [usize], i: usize) -> usize {
     circuits[i]
 }
 
-fn union(circuits: &mut [usize], connection: (usize, usize)) {
-    let (i, j) = connection;
+fn union(conn: Edge, circuits: &mut [usize], sizes: &mut [usize]) {
+    let (i, j) = conn;
     let i_root = find(circuits, i);
     let j_root = find(circuits, j);
     if i_root != j_root {
         circuits[i_root] = j_root;
+        sizes[j_root] += sizes[i_root];
     }
 }
 
-fn part1(coords: &[Coord], pairs: usize) -> Result<usize> {
+fn part1(coords: &[Coord], conns: &[Edge], pairs: usize) -> Result<usize> {
     let _start = Instant::now();
 
-    let edges = connections(coords);
-    let mut circuits: Vec<_> = vec![coords.len(); coords.len()];
+    let mut circuits = vec![coords.len(); coords.len()];
+    let mut sizes = vec![1; coords.len()];
 
-    for &e in edges.iter().take(pairs) {
-        union(&mut circuits, e);
+    for &e in conns.iter().take(pairs) {
+        union(e, &mut circuits, &mut sizes);
     }
 
-    let mut count = HashMap::new();
-    for i in 0..circuits.len() {
-        if circuits[i] == circuits.len() {
-            continue;
+    let mut count = vec![];
+    for i in 0..coords.len() {
+        if circuits[i] == i {
+            count.push(sizes[i]);
         }
-        let e = count.entry(find(&mut circuits, i)).or_insert(0);
-        *e += 1;
     }
 
-    let mut count: Vec<_> = count.values().collect();
     count.sort_by(|a, b| b.cmp(a));
     let size = count[0] * count[1] * count[2];
 
@@ -99,25 +98,16 @@ fn part1(coords: &[Coord], pairs: usize) -> Result<usize> {
     Ok(size)
 }
 
-fn part2(coords: &[Coord]) -> Result<usize> {
+fn part2(coords: &[Coord], conns: &[Edge]) -> Result<usize> {
     let _start = Instant::now();
 
-    let edges = connections(coords);
-
     let mut ans = 0;
-    let mut circuits: Vec<_> = vec![coords.len(); coords.len()];
-    for &(i, j) in edges.iter() {
-        union(&mut circuits, (i, j));
+    let mut circuits = vec![coords.len(); coords.len()];
+    let mut sizes = vec![1; coords.len()];
+    for &(i, j) in conns.iter() {
+        union((i, j), &mut circuits, &mut sizes);
 
-        let mut count = HashMap::new();
-        for i in 0..circuits.len() {
-            circuits[i] = find(&mut circuits, i);
-            *count.entry(circuits[i]).or_insert(0) += 1;
-            if count.len() != 1 {
-                break;
-            }
-        }
-        if count.len() == 1 && count.values().sum::<usize>() == coords.len() {
+        if sizes[find(&mut circuits, j)] == coords.len() {
             ans = coords[i].0 * coords[j].0;
             break;
         }
@@ -132,10 +122,10 @@ fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let coords = parse_input(input)?;
+    let (coords, conns) = parse_input(input)?;
 
-    part1(&coords, 1000)?;
-    part2(&coords)?;
+    part1(&coords, &conns, 1000)?;
+    part2(&coords, &conns)?;
     Ok(())
 }
 
@@ -161,17 +151,17 @@ fn example_input() -> Result<()> {
 862,61,35
 984,92,344
 425,690,689";
-    let coords = parse_input(input)?;
-    assert_eq!(part1(&coords, 10).unwrap(), 40);
-    assert_eq!(part2(&coords).unwrap(), 25272);
+    let (coords, conns) = parse_input(input)?;
+    assert_eq!(part1(&coords, &conns, 10).unwrap(), 40);
+    assert_eq!(part2(&coords, &conns).unwrap(), 25272);
     Ok(())
 }
 
 #[test]
 fn real_input() -> Result<()> {
     let input = std::fs::read_to_string("input/input.txt").unwrap();
-    let coords = parse_input(input)?;
-    assert_eq!(part1(&coords, 1000).unwrap(), 97384);
-    assert_eq!(part2(&coords).unwrap(), 9003685096);
+    let (coords, conns) = parse_input(input)?;
+    assert_eq!(part1(&coords, &conns, 1000).unwrap(), 97384);
+    assert_eq!(part2(&coords, &conns).unwrap(), 9003685096);
     Ok(())
 }
